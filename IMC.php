3c8987cd4ae889ba58427772d243aaa96bf5ae14
Vinícius Sidrim <viniciussidrim99@gmail.com>
2023-08-11 21:13:48 -0300

    Update

--- IMC.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* Efetue um algoritmo PHP que receba a altura e o sexo de uma pessoa,
-calcule e imprima o seu peso ideal, utilizando as seguintes fórmulas:
-• para homens: (72.7 * H) – 58 • para mulheres: (62.1 * H) – 44.7 */
-
-
-
-?>
--- redme.md
+++ redme.md
@@ -1,4 +1,1 @@
-Visto perguntas de tal site
-
-* https://satellasoft.com/artigo/php/lista-de-exercicios-em-php
-* https://informaticaemacao.com/40-exercicios-em-php-e-resolucao/
+Scrips PHP
